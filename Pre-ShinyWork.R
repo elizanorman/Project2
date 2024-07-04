1773635831cfdd3b7fa3243625864097dfd2d731
elizanorman <ecnorma2@ncsu.edu>
2024-07-04 18:19:02 -0400

    Outlining

--- Pre-ShinyWork.R
+++ Pre-ShinyWork.R
@@ -55,3 +55,33 @@ final <- getF1(title = "driverStandings")
 secondTry <- getF1(title = "circuits", year = "2010")
 
 third <- getF1(title = "results", year = 2008)
+
+
+# idea based on practice 1:
+# "Select a year"
+# 1) select year from choices ( selectInput() ) with one option being "All Available"
+# "What category interests you?"
+# 2) buttons like plot type ( radioButtons() )
+#   - options: _results, lap times, standings ?
+# depending on this choice:
+# "Which variables?"
+# 3) radioButtons() again ?
+
+#   numerical variables: give mean per driver and/or location
+#   - ggforce()
+#   - scatter plot ?
+
+#   categorical: (standings ?) give contingency table
+#   - bar plot
+#   - box and whisker for numerical at levels of cat
+
+g <- ggplot(third, aes(x = Class))
+  if(input$plot == "bar"){
+    g + geom_bar()
+  } else if(input$plot == "sideUmemploy"){ 
+    g + geom_bar(aes(fill = as.factor(EmploymentDuration.Unemployed)), position = "dodge") + 
+      scale_fill_discrete(name = "Unemployment status", labels = c("Employed", "Unemployed"))
+  } else if(input$plot == "sideForeign"){
+    g + geom_bar(aes(fill = as.factor(ForeignWorker)), position = "dodge") + 
+      scale_fill_discrete(name = "Status", labels = c("German", "Foreign"))
+  }
