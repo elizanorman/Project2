4c17c13ad77067b0ca372ca5c3104f929350bc56
elizanorman <ecnorma2@ncsu.edu>
2024-07-09 22:41:20 -0400

    Lots working, but no numerical plots

--- server.R
+++ server.R
@@ -72,22 +72,84 @@ shinyServer(function(input, output) {
     outData <- outData |> left_join(color_mappings, by = c("name" = "fruit")) |>
       unnest_wider(col=nutritions, names_sep="_")
     if(input$col == "all fruits"){
-      outData2 <- select(outData, "name", "id", input$vars)
+      if(input$radio == "Nutrition Breakdown"){
+        outData2 <- select(outData, "name", "id", input$varsNut)
+      }else{
+        outData2 <- select(outData, "name", "id", input$varsBio)
+      }
+        
     }else{
-    outData2 <- outData |> 
-      filter(input$col == outData$color) |>
-      select("name", "id", input$vars)
+      if(input$radio == "Nutrition Breakdown"){
+        outData2 <- outData |> 
+          filter(input$col == outData$color) |>
+          select("name", "id", input$varsNut)
+    }else{
+      outData2 <- outData |> 
+        filter(input$col == outData$color) |>
+        select("name", "id", input$varsBio)
     }
+  }
   })
   output$summary <- DT::renderDataTable({
     data2()
 })
   output$download <- downloadHandler(
     filename = function() {
-      "Art.csv"
+      "Fruit.csv"
     },
     content = function(file){
       write.csv(data2(), file, row.names = FALSE)
     }
   )
+  output$table <- renderTable({
+    if(input$numType == "Categorical"){
+    if(input$varsCat == "family vs order"){
+      tab1 <- table(outData$family, outData$order)
+      tab1_df <- as.data.frame(tab1)
+      nonzero_df <- tab1_df[tab1_df$Freq != 0,]
+      nonzero_df1 <- rename(nonzero_df, family=Var1, order=Var2)
+      nonzero_df1
+    }else if(input$varsCat == "order vs genus"){
+      tab1 <- table(outData$order, outData$genus)
+      tab1_df <- as.data.frame(tab1)
+      nonzero_df <- tab1_df[tab1_df$Freq != 0,]
+      nonzero_df1 <- rename(nonzero_df, family=Var1, order=Var2)
+      nonzero_df1
+    }else{
+      tab1 <- table(outData$genus, outData$family)
+      tab1_df <- as.data.frame(tab1)
+      nonzero_df <- tab1_df[tab1_df$Freq != 0,]
+      nonzero_df1 <- rename(nonzero_df, family=Var1, order=Var2)
+      nonzero_df1
+    }
+    }
+  })
+  output$barPlot <- renderPlot({
+    outData <- outData |> left_join(color_mappings, by = c("name" = "fruit")) |>
+      unnest_wider(col=nutritions, names_sep="_")
+    if(input$numType == "Categorical" && input$varsBar == "color"){
+    g <- ggplot(outData, aes(x = color))  
+     g + geom_bar(aes(fill = as.factor(color))) +
+         scale_fill_manual(values = c(blue = "blue", green = "green", orange = "orange", pink = "pink", purple = "purple", red = "red", yellow = "yellow"))
+    }else if(input$numType == "Categorical" && input$varsBar == "family"){
+      g <- ggplot(outData, aes(x = family))  
+      g + geom_bar(aes(fill = as.factor(family)))
+    }else if(input$numType == "Categorical" && input$varsBar == "order"){
+      g <- ggplot(outData, aes(x = order))  
+      g + geom_bar(aes(fill = as.factor(order)))
+    }else if(input$numType == "Categorical" && input$varsBar == "genus"){
+      g <- ggplot(outData, aes(x = genus))  
+      g + geom_bar(aes(fill = as.factor(genus)))
+    }
+  })
+  
+  output$text <- renderText({
+    outData <- outData |> left_join(color_mappings, by = c("name" = "fruit")) |>
+      unnest_wider(col=nutritions, names_sep="_")
+    if(input$numType == "Numeric"){
+      paste("The mean of ", input$numPlots, "is ", round(mean(as.numeric(outData[[input$numPlots]])),2) , ", and the median of ", input$numPlots, "is ", round(median(as.numeric(outData[[input$numPlots]])),2))
+    }
+  })
+  
+  
 })
--- ui.R
+++ ui.R
@@ -30,12 +30,12 @@ shinyUI(fluidPage(
     
     conditionalPanel(
       condition = "input.radio == 'Nutrition Breakdown'",
-      checkboxGroupInput("vars", "Select variables:",
+      checkboxGroupInput("varsNut", "Select variables:",
                          choices = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"), selected = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"))
     ),
     conditionalPanel(
       condition = "input.radio == 'Biological Classification'",
-      checkboxGroupInput("vars", "Select variables:",
+      checkboxGroupInput("varsBio", "Select variables:",
                          choices = c("family", "order", "genus"), selected = c("family", "order", "genus"))
     ),
      downloadButton("download", "Download Dataset")),
@@ -44,5 +44,36 @@ shinyUI(fluidPage(
     mainPanel(
       dataTableOutput("summary")
     ))),
-  tabPanel("Exploration", "")
-)))
+  tabPanel("Exploration",
+           sidebarLayout(
+             sidebarPanel(
+               h4("Are you interested in Numeric or Categorical Summaries?"),
+               radioButtons("numType", "",
+                            choices = c("Numeric", "Categorical")),
+               
+               conditionalPanel(
+                 condition = "input.numType == 'Categorical'",
+                 radioButtons("varsCat", "Contingency tables for which variables:",
+                                    choices = c("family vs order", "order vs genus", "genus vs family"), selected = c("family vs order"))),
+               conditionalPanel(
+                 condition = "input.numType == 'Categorical'",
+                 radioButtons("varsBar", "Bar Plots for which variables:",
+                              choices = c("color", "family", "order", "genus"), selected = c("color"))),
+               conditionalPanel(
+                 condition = "input.numType == 'Numeric'",
+                 radioButtons("numPlots", "Which variable would you like to see numeric summaries for?",
+                                    choices = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"), selected = c("nutritions_calories"))
+               ),
+               conditionalPanel(
+                 condition = "input.numType == 'Numeric'",
+                 checkboxGroupInput("scatter", "Which variables",
+                              choices = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"), selected = c("nutritions_calories"))
+                
+               )),
+            
+           mainPanel(
+             tableOutput("table"),
+             plotOutput("barPlot"),
+             textOutput("text")
+           ))
+))))
