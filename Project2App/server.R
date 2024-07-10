49e4c0fb9eca07c800982743060ecea95d3daacc
elizanorman <ecnorma2@ncsu.edu>
2024-07-10 06:58:21 -0400

    Fixed indentations before 7am

--- Project2App/server.R
+++ Project2App/server.R
@@ -1,3 +1,4 @@
+# load required packages
 library(shiny)
 library(tidyverse)
 library(DT)
@@ -9,92 +10,98 @@ library(ggplot2)
 library(ggExtra)
 
 
-# Define server 
+# Define server logic
 shinyServer(function(input, output) {
-    URL <- "https://fruityvice.com/api/fruit/all"
-    outData <- GET(URL)$content |>
-      rawToChar() |>
-      fromJSON() |>
-      as_tibble()
-    color_mappings <- tribble(
-      ~fruit, ~color,
-      "Strawberry", "red",
-      "Tomato", "red",
-      "Lingonberry", "red",
-      "Lychee", "red",
-      "Raspberry", "red",
-      "Apple", "red",
-      "Pomegranate", "red",
-      "Cranberry", "red",
-      "Cherry", "red",
-      "Durian", "green",
-      "Pear", "green",
-      "Kiwi", "green",
-      "GreenApple", "green",
-      "Melon", "green",
-      "Lime", "green",
-      "Feijoa", "green",
-      "Avocado", "green",
-      "Annona", "green",
-      "Banana", "yellow",
-      "Pineapple", "yellow",
-      "Gooseberry", "yellow",
-      "Lemon", "yellow",
-      "Mango", "yellow",
-      "Kiwifruit", "yellow",
-      "Jackfruit", "yellow",
-      "Hazelnut", "yellow",
-      "Blueberry", "blue",
-      "Ceylon Gooseberry", "blue",
-      "Fig", "pink",
-      "Watermelon", "pink",
-      "Guava", "pink",
-      "Pitahaya", "pink",
-      "Dragonfruit", "pink",
-      "Pomelo", "pink",
-      "Persimmon", "orange",
-      "Orange", "orange",
-      "Apricot", "orange",
-      "Tangerine", "orange",
-      "Peach", "orange",
-      "Horned Melon", "orange",
-      "Pumpkin", "orange",
-      "Japanese Persimmon", "orange",
-      "Papaya", "orange",
-      "Blackberry", "purple",
-      "Passionfruit", "purple",
-      "Plum", "purple",
-      "Grape", "purple",
-      "Morus", "purple",
-      "Mangosteen", "purple"
-    )
+  # access the API and convert content to tibble
+  URL <- "https://fruityvice.com/api/fruit/all"
+  outData <- GET(URL)$content |>
+    rawToChar() |>
+    fromJSON() |>
+    as_tibble()
+  # create a tibble row by row to assign colors to the fruits
+  color_mappings <- tribble(
+    ~fruit, ~color,
+    "Strawberry", "red",
+    "Tomato", "red",
+    "Lingonberry", "red",
+    "Lychee", "red",
+    "Raspberry", "red",
+    "Apple", "red",
+    "Pomegranate", "red",
+    "Cranberry", "red",
+    "Cherry", "red",
+    "Durian", "green",
+    "Pear", "green",
+    "Kiwi", "green",
+    "GreenApple", "green",
+    "Melon", "green",
+    "Lime", "green",
+    "Feijoa", "green",
+    "Avocado", "green",
+    "Annona", "green",
+    "Banana", "yellow",
+    "Pineapple", "yellow",
+    "Gooseberry", "yellow",
+    "Lemon", "yellow",
+    "Mango", "yellow",
+    "Kiwifruit", "yellow",
+    "Jackfruit", "yellow",
+    "Hazelnut", "yellow",
+    "Blueberry", "blue",
+    "Ceylon Gooseberry", "blue",
+    "Fig", "pink",
+    "Watermelon", "pink",
+    "Guava", "pink",
+    "Pitahaya", "pink",
+    "Dragonfruit", "pink",
+    "Pomelo", "pink",
+    "Persimmon", "orange",
+    "Orange", "orange",
+    "Apricot", "orange",
+    "Tangerine", "orange",
+    "Peach", "orange",
+    "Horned Melon", "orange",
+    "Pumpkin", "orange",
+    "Japanese Persimmon", "orange",
+    "Papaya", "orange",
+    "Blackberry", "purple",
+    "Passionfruit", "purple",
+    "Plum", "purple",
+    "Grape", "purple",
+    "Morus", "purple",
+    "Mangosteen", "purple"
+  )
     
-
+  # create a reactive dataset to be used in the summary output
   data2 <- reactive({
+    # merge the color column onto the dataset, and access the columns from nutritions
     outData <- outData |> left_join(color_mappings, by = c("name" = "fruit")) |>
       unnest_wider(col=nutritions, names_sep="_")
+    # allow subsetting by fruit option (col) from the UI
     if(input$col == "all fruits"){
+      # display user's choice of variables from the dataset
       if(input$radio == "Nutrition Facts"){
         outData2 <- select(outData, "name", "id", input$varsNut)
       }else{
         outData2 <- select(outData, "name", "id", input$varsBio)
       }
-        
     }else{
       if(input$radio == "Nutrition Facts"){
         outData2 <- outData |> 
           filter(input$col == outData$color) |>
           select("name", "id", input$varsNut)
-    }else{
-      outData2 <- outData |> 
-        filter(input$col == outData$color) |>
-        select("name", "id", input$varsBio)
+      }else{
+        outData2 <- outData |> 
+          filter(input$col == outData$color) |>
+          select("name", "id", input$varsBio)
+      }
     }
-  }
   })
+  # output the reactive data2()
   output$summary <- DT::renderDataTable({
     data2()
-})
+  })
+  # allow the user to download the reactive data2()
   output$download <- downloadHandler(
     filename = function() {
       "Fruit.csv"
@@ -103,36 +110,38 @@ shinyServer(function(input, output) {
       write.csv(data2(), file, row.names = FALSE)
     }
   )
+  # create a contingency table for categorical data, and only keep nonzero rows
   output$table <- renderTable({
     if(input$numType == "Categorical"){
-    if(input$varsCat == "family vs order"){
-      tab1 <- table(outData$family, outData$order)
-      tab1_df <- as.data.frame(tab1)
-      nonzero_df <- tab1_df[tab1_df$Freq != 0,]
-      nonzero_df1 <- rename(nonzero_df, family=Var1, order=Var2)
-      nonzero_df1
-    }else if(input$varsCat == "order vs genus"){
-      tab1 <- table(outData$order, outData$genus)
-      tab1_df <- as.data.frame(tab1)
-      nonzero_df <- tab1_df[tab1_df$Freq != 0,]
-      nonzero_df1 <- rename(nonzero_df, family=Var1, order=Var2)
-      nonzero_df1
-    }else{
-      tab1 <- table(outData$genus, outData$family)
-      tab1_df <- as.data.frame(tab1)
-      nonzero_df <- tab1_df[tab1_df$Freq != 0,]
-      nonzero_df1 <- rename(nonzero_df, family=Var1, order=Var2)
-      nonzero_df1
-    }
+      if(input$varsCat == "family vs order"){
+        tab1 <- table(outData$family, outData$order)
+        tab1_df <- as.data.frame(tab1)
+        nonzero_df <- tab1_df[tab1_df$Freq != 0,]
+        nonzero_df1 <- rename(nonzero_df, family=Var1, order=Var2)
+        nonzero_df1
+      }else if(input$varsCat == "order vs genus"){
+        tab1 <- table(outData$order, outData$genus)
+        tab1_df <- as.data.frame(tab1)
+        nonzero_df <- tab1_df[tab1_df$Freq != 0,]
+        nonzero_df1 <- rename(nonzero_df, family=Var1, order=Var2)
+        nonzero_df1
+      }else{
+        tab1 <- table(outData$genus, outData$family)
+        tab1_df <- as.data.frame(tab1)
+        nonzero_df <- tab1_df[tab1_df$Freq != 0,]
+        nonzero_df1 <- rename(nonzero_df, family=Var1, order=Var2)
+        nonzero_df1
+      }
     }
   })
+  # create a bar plot for categorical data
   output$barPlot <- renderPlot({
     outData <- outData |> left_join(color_mappings, by = c("name" = "fruit")) |>
       unnest_wider(col=nutritions, names_sep="_")
     if(input$numType == "Categorical" && input$varsBar == "color"){
-    g <- ggplot(outData, aes(x = color))  
-     g + geom_bar(aes(fill = as.factor(color))) +
-         scale_fill_manual(values = c(blue = "blue", green = "green", orange = "orange", pink = "pink", purple = "purple", red = "red", yellow = "yellow"))
+      g <- ggplot(outData, aes(x = color))  
+      g + geom_bar(aes(fill = as.factor(color))) +
+          scale_fill_manual(values = c(blue = "blue", green = "green", orange = "orange", pink = "pink", purple = "purple", red = "red", yellow = "yellow"))
     }else if(input$numType == "Categorical" && input$varsBar == "family"){
       g <- ggplot(outData, aes(x = family))  
       g + geom_bar(aes(fill = as.factor(family)))
@@ -144,18 +153,19 @@ shinyServer(function(input, output) {
       g + geom_bar(aes(fill = as.factor(genus)))
     }
   })
+  # create a line graph for numeric data
   output$line <- renderPlot({
     outData <- outData |> left_join(color_mappings, by = c("name" = "fruit")) |>
       unnest_wider(col=nutritions, names_sep="_")
     if(input$numType == "Categorical"){
       p <- ggplot(outData, aes(x = nutritions_sugar, y = nutritions_calories, color = color)) +
-        geom_line() +
-        labs(x = "Sugar", y = "Calories", color = "Fruit Color") +
-        scale_color_manual(values = c(blue = "blue", green = "green", orange = "orange", pink = "pink", purple = "purple", red = "red", yellow = "yellow"))
+           geom_line() +
+           labs(x = "Sugar", y = "Calories", color = "Fruit Color") +
+           scale_color_manual(values = c(blue = "blue", green = "green", orange = "orange", pink = "pink", purple = "purple", red = "red", yellow = "yellow"))
       p
     }
   })
-  
+  # output numeric summaries (mean and median) for numeric data
   output$text <- renderText({
     outData <- outData |> left_join(color_mappings, by = c("name" = "fruit")) |>
       unnest_wider(col=nutritions, names_sep="_")
@@ -163,28 +173,27 @@ shinyServer(function(input, output) {
       paste("The mean of ", input$numPlots, "is ", round(mean(as.numeric(outData[[input$numPlots]])),2) , ", and the median of ", input$numPlots, "is ", round(median(as.numeric(outData[[input$numPlots]])),2))
     }
   })
-  
+  # output scatter plot, with additional density curves categorized by color on the margins, for numeric data
   output$scatter <- renderPlot({
     outData <- outData |> left_join(color_mappings, by = c("name" = "fruit")) |>
       unnest_wider(col=nutritions, names_sep="_")
     if(input$numType == "Numeric"){
       f <- ggplot(outData, aes(x = as.numeric(.data[[input$scatterVar1]]), y = as.numeric(.data[[input$scatterVar2]]), colour = as.factor(.data$color))) +
-        geom_point()+
-        labs(x = input$scatterVar1, y = input$scatterVar2, colour = "Color")
-      ggMarginal(f, groupColour = TRUE, groupFill = TRUE)
+           geom_point()+
+           labs(x = input$scatterVar1, y = input$scatterVar2, colour = "Color")
+           ggMarginal(f, groupColour = TRUE, groupFill = TRUE)
     }
   })
-
+  # output boxplot for calories by fruit color
   output$box <- renderPlot({
     outData <- outData |> left_join(color_mappings, by = c("name" = "fruit")) |>
       unnest_wider(col=nutritions, names_sep="_")
     if(input$numType == "Numeric"){
       b <- ggplot(outData, aes(x = color, y = nutritions_calories, fill = as.factor(color))) +
-        geom_boxplot() +
-        scale_fill_manual(values = c(blue = "blue", green = "green", orange = "orange", pink = "pink", purple = "purple", red = "red", yellow = "yellow")) +
-        labs(x = "Color", y = "Calories", fill = "Color")
+           geom_boxplot() +
+           scale_fill_manual(values = c(blue = "blue", green = "green", orange = "orange", pink = "pink", purple = "purple", red = "red", yellow = "yellow")) +
+           labs(x = "Color", y = "Calories", fill = "Color")
       b
     }
   })
-  
 })
--- Project2App/ui.R
+++ Project2App/ui.R
@@ -1,3 +1,4 @@
+# load required packages
 library(shiny)
 library(DT)
 library(jsonlite)
@@ -9,91 +10,92 @@ library(ggplot2)
 library(ggExtra)
 
 
-# Define UI for application that draws a histogram
+# Define UI for application
 shinyUI(fluidPage(
-  
   # Application title
   titlePanel("Fruit Nutrition Data"),
+  # create first 'About' tab
   tabsetPanel(
     tabPanel("About",
-       sidebarLayout(
+      sidebarLayout(
         sidebarPanel(
           h3("This data set comes from the ", a(href = "https://www.fruityvice.com/", "Fruityvice API")),
           br(),
           h4("From the API:"),
           h5("With Fruityvice you can receive interesting data from any fruit of your choosing. On top of that you can add fruits by yourself as well! Added fruits will first have to be approved by an admin to avoid any errors in the data. The shown data is based on 100 grams of the listed fruit. The owner does not guarantee the available data is 100% flawless, however he will do his best to fix any wrong data.")
                  
-    ),
+        ),
         mainPanel(
           img(src = "cherry.png" )
-        ))),
-  # Sidebar with a slider input for number of bins
-  tabPanel("Data Download",
-  sidebarLayout(
-    sidebarPanel(
-      h4("What color fruit would you like to know about?"),
-      selectInput("col", label = "Colors",
+        )
+      )
+    ),
+  # create second tab
+    tabPanel("Data Download",
+      sidebarLayout(
+        sidebarPanel(
+          h4("What color fruit would you like to know about?"),
+          selectInput("col", label = "Colors",
                   choices = c( "all fruits", "red", "blue", "green", "yellow", "pink", "orange", "purple"),
                   selected = "all fruits"),
-      br(),
-    radioButtons("radio", "Nutrition or Biology:",
-                 choices = c("Nutrition Facts", "Biological Classification")),
+          br(),
+          radioButtons("radio", "Nutrition or Biology:",choices = c("Nutrition Facts", "Biological Classification")
+            ),
     
-    conditionalPanel(
-      condition = "input.radio == 'Nutrition Facts'",
-      checkboxGroupInput("varsNut", "Select variables:",
-                         choices = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"), selected = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"))
+          conditionalPanel(
+            condition = "input.radio == 'Nutrition Facts'",
+            checkboxGroupInput("varsNut", "Select variables:",choices = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"), selected = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"))
+          ),
+          conditionalPanel(
+            condition = "input.radio == 'Biological Classification'",
+            checkboxGroupInput("varsBio", "Select variables:",choices = c("family", "order", "genus"), selected = c("family", "order", "genus"))
+          ),
+          downloadButton("download", "Download Dataset")
+        ),
+        # Show the generated dataset
+        mainPanel(
+          dataTableOutput("summary")
+        )
+      )
     ),
-    conditionalPanel(
-      condition = "input.radio == 'Biological Classification'",
-      checkboxGroupInput("varsBio", "Select variables:",
-                         choices = c("family", "order", "genus"), selected = c("family", "order", "genus"))
-    ),
-     downloadButton("download", "Download Dataset")),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      dataTableOutput("summary")
-    ))),
-  tabPanel("Data Exploration",
-           sidebarLayout(
-             sidebarPanel(
-               h4("Are you interested in Numeric or Categorical Summaries?"),
-               radioButtons("numType", "",
-                            choices = c("Numeric", "Categorical")),
+    # create third tab
+    tabPanel("Data Exploration",
+      sidebarLayout(
+        sidebarPanel(
+          h4("Are you interested in Numeric or Categorical Summaries?"),
+          radioButtons("numType", "",choices = c("Numeric", "Categorical")
+          ),
                
-               conditionalPanel(
-                 condition = "input.numType == 'Categorical'",
-                 radioButtons("varsCat", "Contingency tables for which variables:",
-                                    choices = c("family vs order", "order vs genus", "genus vs family"), selected = c("family vs order"))),
-               conditionalPanel(
-                 condition = "input.numType == 'Categorical'",
-                 radioButtons("varsBar", "Select a variable for the bar plot:",
-                              choices = c("color", "family", "order", "genus"), selected = c("color"))),
-               conditionalPanel(
-                 condition = "input.numType == 'Numeric'",
-                 radioButtons("numPlots", "Select a variable for numeric summaries:",
-                                    choices = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"), selected = c("nutritions_calories"))
-               ),
-               conditionalPanel(
-                 condition = "input.numType == 'Numeric'",
-                 selectInput("scatterVar1", "Select the first numeric variable for plotting",
-                              choices = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"), selected = "nutritions_calories")
-                
-               ),
-               conditionalPanel(
-                 condition = "input.numType == 'Numeric'",
-                 selectInput("scatterVar2", "Select the second numeric variable for plotting",
-                             choices = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"), selected = "nutritions_fat")
-                 
-               )),
-            
-           mainPanel(
-             tableOutput("table"),
-             plotOutput("barPlot"),
-             plotOutput("line"),
-             textOutput("text"),
-             plotOutput("scatter"),
-             plotOutput("box")
-           ))
-))))
+          conditionalPanel(
+            condition = "input.numType == 'Categorical'",
+            radioButtons("varsCat", "Contingency tables for which variables:",choices = c("family vs order", "order vs genus", "genus vs family"), selected = c("family vs order"))
+          ),
+          conditionalPanel(
+            condition = "input.numType == 'Categorical'",
+            radioButtons("varsBar", "Select a variable for the bar plot:",choices = c("color", "family", "order", "genus"), selected = c("color"))
+          ),
+          conditionalPanel(
+            condition = "input.numType == 'Numeric'",
+            radioButtons("numPlots", "Select a variable for numeric summaries:",choices = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"), selected = c("nutritions_calories"))
+          ),
+          conditionalPanel(
+            condition = "input.numType == 'Numeric'",
+            selectInput("scatterVar1", "Select the first numeric variable for plotting",choices = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"), selected = "nutritions_calories")
+          ),
+          conditionalPanel(
+            condition = "input.numType == 'Numeric'",
+            selectInput("scatterVar2", "Select the second numeric variable for plotting",choices = c("nutritions_calories", "nutritions_fat", "nutritions_sugar", "nutritions_carbohydrates", "nutritions_protein"), selected = "nutritions_fat")
+          )
+        ),
+        mainPanel(
+          tableOutput("table"),
+          plotOutput("barPlot"),
+          plotOutput("line"),
+          textOutput("text"),
+          plotOutput("scatter"),
+          plotOutput("box")
+        )
+      )
+    )
+  )
+))
